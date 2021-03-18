07913eaa818b2a5d3f6116b1cf7164bc997b0561
AlanKev117 <fuentes.alan.98@gmail.com>
2021-03-17 22:02:23 -0600

    Primeros pasos nodo sensor

---
+++ sensor_node/node.py
@@ -0,0 +1,11 @@
+import json
+
+import click
+from gpiozero import DigitalInputDevice
+
+
+def main(sensor_type, sensor_name, sensor_pin, led_pin):
+    pass
+
+if __name__ == "__main__":
+    main()
