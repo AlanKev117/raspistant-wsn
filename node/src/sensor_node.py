84c58c3acce229d1d0cecdf0e934169ae0ef1725
AlanKev117 <fuentes.alan.98@gmail.com>
2021-05-13 20:50:08 -0500

    Correccion de argumentos de main sensor node

--- node/src/sensor_node.py
+++ node/src/sensor_node.py
@@ -16,29 +16,29 @@ from node.src.node_service import SensorNodeService
 from node.src.sensor import DummySensor, HallSensor, PIRSensor
 
 @click.command()
-@click.option('--name', default="aleatorio",
+@click.option('--node-name', default="aleatorio",
               metavar='<nombre del sensor>', show_default=True,
               help='Nombre con el que se llamará al nodo desde el asistente')
-@click.option('--type', default="dummy",
+@click.option('--sensor-type', default="dummy",
               metavar='<tipo de sensor>', show_default=True,
               help='Tipo de sensor que se conectará al nodo.')
 @click.option('--port', default=18861,
               metavar='<puerto>', show_default=True,
               help='Puerto por el que el nodo recibe peticiones de medición')
 @click.option('--verbose', '-v', is_flag=True, help='Modo verbose')
-def main(sensor_name, sensor_type, server_port, verbose):
+def main(node_name, sensor_type, port, verbose):
     logging.basicConfig(level=logging.INFO if verbose else logging.ERROR)
-    sensor_node_process(sensor_name, sensor_type, server_port)
+    sensor_node_process(node_name, sensor_type, port)
 
 
-def sensor_node_process(name, type, port):
+def sensor_node_process(node_name, sensor_type, port):
     sensor_types = {
         "dummy": DummySensor,
         "pir": PIRSensor,
         "hall": HallSensor
     }
 
-    sensor = sensor_types[type](name)
+    sensor = sensor_types[sensor_type](node_name)
     service = classpartial(SensorNodeService, sensor)
 
     hilo_internet = threading.Thread(
@@ -47,7 +47,7 @@ def sensor_node_process(name, type, port):
 
     t = ThreadedServer(service, port=port,
                        registrar=UDPRegistryClient(timeout=0))
-    logging.info(f"Nodo sensor {name} iniciado.")
+    logging.info(f"Nodo sensor {node_name} iniciado.")
     t.start()
     sensor.deactivate()
 
