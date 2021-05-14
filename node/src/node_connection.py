38f7bc4ae4f2ab549e56cfca2f655a0573b17842
AlanKev117 <fuentes.alan.98@gmail.com>
2021-05-14 11:37:26 -0500

    Polling de conexion de nodo mejorado

--- node/src/node_connection.py
+++ node/src/node_connection.py
@@ -6,8 +6,9 @@ from gpiozero import LED
 
 CONNECTION_LED_PIN = 25
 
+
 def check_node_connection():
-    
+
     logging.info("Hilo de conexion a red local iniciado")
 
     connected = False
@@ -16,10 +17,14 @@ def check_node_connection():
     led = LED(CONNECTION_LED_PIN)
 
     while True:
-        
+
+        # Tiempo de intervalo de polling e inicio de systemd
+        time.sleep(5)
+
+        # Polling de conexión local
         ip = subprocess.check_output("hostname -I",
-                                        stderr=subprocess.STDOUT,
-                                        shell=True).decode()
+                                     stderr=subprocess.STDOUT,
+                                     shell=True).decode()
         if ip == "\n":
             changed = connected
             connected = False
@@ -29,17 +34,12 @@ def check_node_connection():
 
         # Actualiza indicadores de conexión
         if first_time or changed:
-            
+
             if connected:
                 logging.info("Conectado a internet!")
                 led.on()
             else:
                 logging.error("Sin conexion a internet")
                 led.off()
-            
+
             first_time = False
-
-
-        time.sleep(5)
-
-    
