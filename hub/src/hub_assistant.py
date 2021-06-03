470a70ebecaf58ccc647fb2a7db601cd2f501fd8
noeav <38843222+noeav@users.noreply.github.com>
2021-06-02 21:25:47 -0500

    Update hub_assistant.py

--- hub/src/hub_assistant.py
+++ hub/src/hub_assistant.py
@@ -1,6 +1,4 @@
 # Copyright 2021 Alan Fuentes
-# Copyright 2021 Yael Estrada
-# Copyright 2021 Noé Acosta
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 """ Este código implementa un cliente gRPC para el API de Asistente de Google
 
